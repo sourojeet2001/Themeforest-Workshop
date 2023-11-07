0dd58e9167ee9e427394717c83819ea64c8f57f9
sourojeet2001 <sourojeet.paul@innoraft.com>
2023-11-07 17:31:50 +0530

    FTC-7: Completed Static Design.
    1. Completes the static design of the template.
    2. Adds Proper Style.
    3. Refactored CSS into SCSS.

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+let gulp = require('gulp'),
+sass = require('gulp-dart-sass'),
+cleanCSS = require('gulp-clean-css');
+rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  styles: {
+    src: './scss/**/*.scss',
+    dest: './dist/css/'
+  }
+};
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: true,
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(cleanCSS())
+    .pipe( rename( { suffix: '.min' } ) )
+    .pipe(sourcemaps.write('./dist/css/'))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function watch() {
+  gulp.watch(paths.styles.src, styles);
+}
+
+var build = gulp.series(gulp.parallel(styles));
+exports.styles = styles;
+exports.watch = watch;
+exports.build = build;
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;
